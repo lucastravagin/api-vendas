6e059f48aa7233cd320ea7005920896e784a93f3
Lucas Travagin <lucas_travagin1112@hotmail.com>
2018-04-05 08:36:22 -0300

    Testando a primeira classe de dominio - Categoria

---
+++ src/main/java/com/lucastravagin/backendapi/domain/Categoria.java
@@ -0,0 +1,63 @@
+package com.lucastravagin.backendapi.domain;
+
+import java.io.Serializable;
+
+public class Categoria implements Serializable {
+	// versão da classe
+	private static final long serialVersionUID = 1L;
+	
+	private int id;
+	private String nome;
+	
+	//Método Construtor
+	public Categoria() {	
+	}
+
+	public Categoria(int id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+	
+	//Metódos de acesso para os atributos da classe (Encapsulamento)
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+	
+	//HashCode e Equals (Implementação padrão, somente ID)
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
+	
+}
