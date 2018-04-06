5c50cc5c49344c9d61b199ec6fcc24531dce5677
Lucas Travagin <lucas_travagin1112@hotmail.com>
2018-04-05 22:04:51 -0300

    Criando uma operação de instanciação

--- src/main/java/com/lucastravagin/backendapi/BackendApiApplication.java
+++ src/main/java/com/lucastravagin/backendapi/BackendApiApplication.java
@@ -1,14 +1,34 @@
 package com.lucastravagin.backendapi;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class BackendApiApplication {
+import com.lucastravagin.backendapi.domain.Categoria;
+import com.lucastravagin.backendapi.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class BackendApiApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApiApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
+	}
+	
 }
 
 
--- src/main/java/com/lucastravagin/backendapi/domain/Categoria.java
+++ src/main/java/com/lucastravagin/backendapi/domain/Categoria.java
@@ -24,7 +24,7 @@ public class Categoria implements Serializable {
 	public Categoria() {	
 	}
 
-	public Categoria(int id, String nome) {
+	public Categoria(Integer id, String nome) {
 		super();
 		this.id = id;
 		this.nome = nome;
